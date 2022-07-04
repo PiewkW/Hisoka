dd2eee587d79d98388c16a6c7982a2c3eab98a5b
PiewkW <75357006+PiewkW@users.noreply.github.com>
2022-07-04 23:04:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -38,7 +38,7 @@ global.mess = {
 }
 global.limitawal = {
     premium: "Infinity",
-    free: 100
+    free: "Infinity"
 }
 global.thumb = fs.readFileSync('./lib/hisoka.jpg')
 global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
